437b8b644c1d0462af085c5b08e2db27ff309987
Shii <ziyiashii@gmail.com>
2025-04-09 18:01:08 +0900

    UIの表示

---
+++ Source/BicycleProject/Private/WuBranch/UI/QuestionUIActor.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/UI/QuestionUIActor.h"
+#include "Components/BoxComponent.h"
+#include <WuBranch/Bike/BikeComponent.h>
+#include <WuBranch/MyGameInstance.h>
+#include "WuBranch/Device/DeviceManager.h"
+#include "WuBranch/Device/Device.h"
+#include <Kismet/KismetSystemLibrary.h>
+
+AQuestionUIActor::AQuestionUIActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	_temporaryParkingArea = CreateDefaultSubobject<UBoxComponent>(FName("parkingArea"));
+	_temporaryParkingArea->SetupAttachment(RootComponent);
+	_temporaryParkingArea->SetRelativeLocation(FVector(100.0f, 0.0f, 0.0f));
+	_temporaryParkingArea->InitBoxExtent(FVector(100.0f, 32.0f, 5.0f));
+	_temporaryParkingArea->OnComponentBeginOverlap.AddDynamic(this, &AQuestionUIActor::OnOverlapBeginParkingArea);
+	AddInstanceComponent(_temporaryParkingArea);
+}
+
+void AQuestionUIActor::HandlePlayerEnterArea(UBikeComponent* bike)
+{
+	// 自転車のスピードを強制的に0まで下げる
+	bike->OpenForcedControl();
+	bike->ReduceVelocityTo0();
+
+	// プレイヤーに答えを選べるようにする
+	UMyGameInstance* gameInstance = Cast<UMyGameInstance>(GetWorld()->GetGameInstance());
+	gameInstance->GetDeviceManager()->EnableSelectAnswerActions();
+	gameInstance->GetDeviceManager()->BindSelectLeftEvent(bike, "OnSelectLeftAnswer");
+	gameInstance->GetDeviceManager()->BindSelectRightEvent(bike, "OnSelectRightAnswer");
+}
+
+void AQuestionUIActor::OnOverlapBeginParkingArea(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag("Player"))
+	{
+		UKismetSystemLibrary::PrintString(this, "Start enter parking area", true, false, FColor::Red, 10.f);
+		UBikeComponent* bike = OtherActor->GetComponentByClass<UBikeComponent>();
+		HandlePlayerEnterArea(bike);
+		// エリアのコリジョンを機能させない
+		_temporaryParkingArea->SetCollisionEnabled(ECollisionEnabled::Type::NoCollision);
+	}
+}
--- Source/BicycleProject/Public/WuBranch/Bike/BikeComponent.h
+++ Source/BicycleProject/Public/WuBranch/Bike/BikeComponent.h
@@ -25,7 +25,27 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	/// <summary>
+	/// 強制的にコントロールをオンにする
+	/// </summary>
+	void OpenForcedControl();
+
+	/// <summary>
+	/// 強制的にコントロールをオフにする
+	/// </summary>
+	void CloseForcedControl();
+
+	/// <summary>
+	/// スピードを0まで下げる
+	/// </summary>
+	void ReduceVelocityTo0();
+
 private:
+
+	/// <summary>
+	/// 慣性の処理
+	/// </summary>
+	void HandleInertia(float DeltaTime);
 
 	/// <summary>
 	/// 移動
@@ -33,6 +53,33 @@ private:
 	/// <param name="direction">移動方向</param>
 	UFUNCTION()
 	void OnMove(FVector2D direction);
+
+	/// <summary>
+	/// VRデバイスに接続しているか
+	/// </summary>
+	/// <returns>true: はい / false: いいえ</returns>
+	bool IsVRConnect() const;
+	/// 左の答えを選ぶ
+	/// </summary>
+	UFUNCTION()
+	void OnSelectLeftAnswer();
+
+	/// <summary>
+	/// 右の答えを選ぶ
+	/// </summary>
+	UFUNCTION()
+	void OnSelectRightAnswer();
+
+	/// <summary>
+	/// 答えを選んだの処理
+	/// </summary>
+	/// <param name="dir">曲がりたい方向</param>
+	void HandleSelectAnswer(FRotator dir);
+
+	/// <summary>
+	/// 答えを選ぶ動作を機能させない
+	/// </summary>
+	void DisableSelectAnswer();
 
 	/// <summary>
 	/// デバイスマネージャー
@@ -48,5 +95,23 @@ private:
 	/// <summary>
 	/// 単位速度
 	/// </summary>
-	const float _unitSpeed = 10000;
+	const float _unitSpeed = 1;
+
+	/// <summary>
+	/// 強制的にコントロールのスウィッチ
+	/// </summary>
+	bool _isForcedControl;
+
+	/// <summary>
+	/// 慣性の速度
+	/// </summary>
+	FVector _inertiaVelocity;
+
+	/// <summary>
+	/// 慣性の減衰
+	/// </summary>
+	UPROPERTY(EditAnywhere, Category = "Bike", meta = (AllowPrivateAccess = "true"))
+	float _inertiaDamping;
+
+	class UCapsuleComponent* _player;
 };

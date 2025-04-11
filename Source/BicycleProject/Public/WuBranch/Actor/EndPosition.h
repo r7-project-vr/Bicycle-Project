cbb62208c76ea6b92bb3d49abb1b54e59d0ce753
gene <cg100111@gmail.com>
2025-04-11 21:00:22 +0900

    スタート地点とエンド地点を追加した

---
+++ Source/BicycleProject/Public/WuBranch/Actor/EndPosition.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "EndPosition.generated.h"
+
+UCLASS()
+class BICYCLEPROJECT_API AEndPosition : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEndPosition();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UFUNCTION()
+	void OnOverlapBeginFinishLine(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	/// <summary>
+	/// 今の世界を再読み込み
+	/// </summary>
+	void RestartLevel();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* _mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* _finishLineCollision;
+
+	bool _isReadyToChangeLevel;
+};

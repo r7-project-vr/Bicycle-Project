0d81de173990b7c46529e1f78ee92e1218bb369a
boxS0225 <167198715+Be-konjouyu@users.noreply.github.com>
2025-04-16 20:15:14 +0900

    UIのBPをC++に変更

---
+++ Source/BicycleProject/Public/ShiiBranch/OptionUIWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "OptionUIWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract, Blueprintable)
+class BICYCLEPROJECT_API UOptionUIWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SetQuestion(const FString& quest);
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void SetAnswer(const FString& answer);
+
+};

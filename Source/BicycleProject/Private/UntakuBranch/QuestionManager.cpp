a1b4b33275772280fa665e58176f979a7a40fc64
MurasameZincin <wangyunze449129954@gmail.com>
2025-05-27 21:48:49 +0900

    問題抽出テストfunction
    問題なし

--- Source/BicycleProject/Private/UntakuBranch/QuestionManager.cpp
+++ Source/BicycleProject/Private/UntakuBranch/QuestionManager.cpp
@@ -22,7 +22,11 @@ AQuestionManager::AQuestionManager()
 // Called when the game starts or when spawned
 void AQuestionManager::BeginPlay()
 {
+
+	UE_LOG(LogTemp, Warning, TEXT("BeginPlay triggered!"));
 	Super::BeginPlay();
+
+	TestRandomQuestions();
 	
 }
 
@@ -61,10 +65,22 @@ void AQuestionManager::TestRandomQuestions()
 	int32 NumToDraw = 3;//自由設定できる　抽選問題数
 	TArray<FQuestion> RandomQuestions = GetRandomQuestions(NumToDraw);
 
+	int32 MessageKey = 0;
+
 	for (const FQuestion& Q : RandomQuestions)
 	{
-		FString Message = FString::Printf(TEXT("Get Question ID: %d CONTENT: %s"), Q.ID, *Q.Content);
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, Message);
+		GEngine->AddOnScreenDebugMessage(MessageKey++, 8.f, FColor::Cyan,
+			FString::Printf(TEXT("[QuestionID: %d] %s"), Q.ID, *Q.Content));
+
+		for (int32 i = 0; i < Q.AnswerContents.Num(); ++i)
+		{
+			TCHAR OptionChar = 'A' + i;
+			FString OptionStr = FString::Printf(TEXT("%c. %s "), OptionChar, *Q.AnswerContents[i]);
+			GEngine->AddOnScreenDebugMessage(MessageKey++, 8.f, FColor::White, OptionStr);
+		}
+
+		GEngine->AddOnScreenDebugMessage(MessageKey++, 8.f, FColor::Green,
+			FString::Printf(TEXT("CorrectAnswer: %d"), Q.Correct));
 	}
 
 	

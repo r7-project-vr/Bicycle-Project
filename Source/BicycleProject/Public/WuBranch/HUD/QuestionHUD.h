02aec96517839848a48faac2e082be811025fc3e
gene <cg100111@gmail.com>
2025-07-11 01:58:30 +0900

    基本のHUDを作った

---
+++ Source/BicycleProject/Public/WuBranch/HUD/QuestionHUD.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "QuestionHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BICYCLEPROJECT_API AQuestionHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+
+	AQuestionHUD();
+
+	virtual void BeginPlay() override;
+
+	/// <summary>
+	/// HUDのウィジェットをゲット
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <returns>ウィジェット</returns>
+	template<class T>
+	T* GetHUDWidget()
+	{
+		return Cast<T>(HUDWidget);
+	}
+
+private:
+
+	/// <summary>
+	/// HUDUIを生成して画面に追加
+	/// </summary>
+	void AddWidgetToView();
+
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UUserWidget> HUDUIClass;
+
+	/// <summary>
+	/// HUD Widget
+	/// </summary>
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	UUserWidget* HUDWidget;
+};

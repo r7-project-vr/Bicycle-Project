b39e8a003fc603a5eac548018565a010cd0bc383
gene <cg100111@gmail.com>
2025-07-26 09:12:47 +0900

    有線デバイスの基本実装すべきものを簡単に作った

---
+++ Source/BicycleProject/Private/WuBranch/Device/WiredDevice.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/Device/WiredDevice.h"
+
+UWiredDevice::UWiredDevice()
+	: MoveSwitch(false)
+	, SelectSwitch(false)
+{
+}
+
+void UWiredDevice::Init()
+{
+	EnableMoveAction_Implementation();
+}
+
+bool UWiredDevice::Connect()
+{
+	return false;
+}
+
+bool UWiredDevice::Disconnect()
+{
+	return false;
+}
+
+void UWiredDevice::EnableMoveAction_Implementation()
+{
+	MoveSwitch = true;
+}
+
+void UWiredDevice::DisableMoveAction_Implementation()
+{
+	MoveSwitch = false;
+}
+
+void UWiredDevice::EnableSelectAnswerAction_Implementation()
+{
+	SelectSwitch = true;
+}
+
+void UWiredDevice::DisableSelectAnswerAction_Implementation()
+{
+	SelectSwitch = false;
+}
+
+void UWiredDevice::FindDevice()
+{
+}
+
---
+++ Source/BicycleProject/Public/WuBranch/Device/WiredDevice.h
@@ -0,0 +1,51 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WuBranch/Device/Device.h"
+#include "WiredDevice.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BICYCLEPROJECT_API UWiredDevice : public UDevice
+{
+	GENERATED_BODY()
+	
+public:
+
+	UWiredDevice();
+
+	virtual void Init() override;
+
+	virtual bool Connect() override;
+
+	virtual bool Disconnect() override;
+
+	void EnableMoveAction_Implementation() override;
+
+	void DisableMoveAction_Implementation() override;
+
+	void EnableSelectAnswerAction_Implementation() override;
+
+	void DisableSelectAnswerAction_Implementation() override;
+
+private:
+
+	/// <summary>
+	/// デバイスを探す
+	/// </summary>
+	void FindDevice();
+
+	/// <summary>
+	/// 移動機能のスイッチ
+	/// </summary>
+	bool MoveSwitch;
+
+	/// <summary>
+	/// 選ぶ機能のスイッチ
+	/// </summary>
+	bool SelectSwitch;
+};

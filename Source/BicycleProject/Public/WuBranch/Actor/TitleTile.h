5cd0734d82d1cef617ff7ae1237c92c87c9b6cf8
gene <cg100111@gmail.com>
2025-08-25 09:10:21 +0900

    タイトル画面用のTileを作った コメントをつけた

---
+++ Source/BicycleProject/Public/WuBranch/Actor/TitleTile.h
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TitleTile.generated.h"
+
+class UBoxComponent;
+class UEnvironmentalObjectComponent;
+class URandomFoliageSpawner;
+
+UCLASS()
+class BICYCLEPROJECT_API ATitleTile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATitleTile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	/// <summary>
+	/// 地面
+	/// </summary>
+	UStaticMeshComponent* RoadMesh;
+
+	/// <summary>
+	/// 次のTileを生成する位置
+	/// </summary>
+	UBoxComponent* NextTileSpawnPoint;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	UBoxComponent* TileDeletePoint;
+
+
+	/// <summary>
+	/// 建物のメッシュ
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TArray<UEnvironmentalObjectComponent*> Buildings;
+
+	/// <summary>
+	/// フォリッジ生成者
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	URandomFoliageSpawner* FoliageSpawner = nullptr;
+};

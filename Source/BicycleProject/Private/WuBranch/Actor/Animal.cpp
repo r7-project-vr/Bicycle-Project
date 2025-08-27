00ae3f461a9ef95614c306125f22cb7300308315
gene <cg100111@gmail.com>
2025-08-28 02:39:05 +0900

    動物を作ってみた(未完成)

---
+++ Source/BicycleProject/Private/WuBranch/Actor/Animal.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/Actor/Animal.h"
+#include "GameFramework/Character.h"
+
+// Sets default values
+AAnimal::AAnimal()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AAnimal::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAnimal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	Chase();
+	
+}
+
+void AAnimal::Chase()
+{
+	if (Target.IsNull())
+		return;
+
+	FVector MyLocation = GetActorLocation();
+	FVector TargetLocation = Target.Get()->GetActorLocation();
+	float TotalDistance = FVector::DistXY(MyLocation, TargetLocation);
+
+	// 一定以上の距離を離れたら追う
+	if (TotalDistance >= StartChaseDistance)
+	{
+		float DeltaDistance = TotalDistance * ChaseDistancePerFrame / 100;
+		FVector Direction = TargetLocation - MyLocation;
+
+		// 向き変更
+		SetActorRotation(Direction.Rotation());
+		// 座標
+		SetActorLocation(GetActorLocation() + Direction.GetSafeNormal() * DeltaDistance);
+	}
+}
+
---
+++ Source/BicycleProject/Public/WuBranch/Actor/Animal.h
@@ -0,0 +1,52 @@
+﻿
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Animal.generated.h"
+
+UCLASS()
+class BICYCLEPROJECT_API AAnimal : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAnimal();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	/// <summary>
+	/// 追う
+	/// </summary>
+	void Chase();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* Mesh;
+
+	/// <summary>
+	/// 追う目標
+	/// </summary>
+	TObjectPtr<ACharacter> Target;
+
+	/// <summary>
+	/// 追い始まる距離
+	/// </summary>
+	float StartChaseDistance;
+
+	/// <summary>
+	/// 毎フレーム追う距離(%)
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	float ChaseDistancePerFrame;
+};

7a73454e1b414cfee98df0ab9fa913ec17570620
ウー <cg100111@gmail.com>
2025-09-05 12:24:26 +0900

    アクターを表示するコントローラ

---
+++ Source/BicycleProject/Private/WuBranch/Actor/Component/DisplayComponent.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WuBranch/Actor/Component/DisplayComponent.h"
+#include "Engine/DirectionalLight.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/DirectionalLight.h"
+
+// Sets default values for this component's properties
+UDisplayComponent::UDisplayComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	//PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDisplayComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	//UGameplayStatics::GetAllActorsOfClassWithTag(GetWorld(), );
+}
+
+// Called every frame
+//void UDisplayComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+//{
+//	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+//
+//	// ...
+//}
+
+
+void UDisplayComponent::UpdateTime(FMyTime Time)
+{
+	if(Time.hour24)
+}
---
+++ Source/BicycleProject/Public/WuBranch/Actor/Component/DisplayComponent.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WuBranch/Struct/MyTime.h"
+#include "DisplayComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BICYCLEPROJECT_API UDisplayComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDisplayComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	//virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+
+	/// <summary>
+	/// 時間の更新
+	/// </summary>
+	/// <param name="Time">時間</param>
+	UFUNCTION()
+	void UpdateTime(FMyTime Time);
+
+	/// <summary>
+	/// 
+	/// </summary>
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	FMyTime ShowTime;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	FMyTime HideTime;
+};

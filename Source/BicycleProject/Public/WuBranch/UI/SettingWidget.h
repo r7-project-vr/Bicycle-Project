3533594a9402edcc36ec23ae31dfe5aa04e8f9cb
ウー <cg100111@gmail.com>
2025-09-10 14:35:04 +0900

    コインの高さを調整するUI (未完成)

---
+++ Source/BicycleProject/Public/WuBranch/UI/SettingWidget.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SettingWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BICYCLEPROJECT_API USettingWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	/// <summary>
+	/// 測定開始の通知
+	/// </summary>
+	/// <param name=""></param>
+	DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStartRecalibrationDelegate);
+
+	FStartRecalibrationDelegate OnStartRecalibrationEvent;
+
+};

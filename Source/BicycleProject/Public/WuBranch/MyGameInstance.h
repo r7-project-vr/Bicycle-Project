e7bc89ebd59989486b86ebb4c8f718e7be1b3d47
gene <cg100111@gmail.com>
2025-12-05 02:24:33 +0900

    ファイルの読み書き機能を作った(バッグあり)

--- Source/BicycleProject/Public/WuBranch/MyGameInstance.h
+++ Source/BicycleProject/Public/WuBranch/MyGameInstance.h
@@ -9,6 +9,7 @@
 class UDeviceManager;
 struct FQuestion;
 class AAnimal;
+struct FPlayerSaveGame;
 
 /**
  * 
@@ -117,12 +118,12 @@ private:
 	/// <summary>
 	/// コインをファイルに保存
 	/// </summary>
-	void SaveCoinsToFile();
+	void SaveCoinsToFile(FPlayerSaveGame* Data);
 
 	/// <summary>
 	/// ファイルからコインを読み込む
 	/// </summary>
-	void ReadCoinFromFile();
+	void ReadCoinFromFile(FPlayerSaveGame* Data);
 
 	/// <summary>
 	/// コインの数を更新
@@ -261,6 +262,18 @@ private:
 	void NotifyUpdateRPM();
 
 	/// <summary>
+	/// RPMデータをファイルに保存
+	/// </summary>
+	/// <param name="Data"></param>
+	void SaveRPMToFile(FPlayerSaveGame* Data);
+
+	/// <summary>
+	/// ファイルからRPMデータをゲット
+	/// </summary>
+	/// <param name="Data"></param>
+	void ReadRPMFromFile(FPlayerSaveGame* Data);
+
+	/// <summary>
 	/// 回転数の標準値
 	/// </summary>
 	int StandardRPM;
@@ -296,6 +309,21 @@ public:
 	int32 GetNumOfSets() const;
 
 private:
+
+	// 2025.12.05 ウー start
+	/// <summary>
+	/// セット数をファイルに保存
+	/// </summary>
+	/// <param name="Data"></param>
+	void SaveSetsToFile(FPlayerSaveGame* Data);
+
+	/// <summary>
+	/// ファイルからセット数をゲット
+	/// </summary>
+	/// <param name="Data"></param>
+	void ReadSetsFromFile(FPlayerSaveGame* Data);
+	// 2025.12.05 ウー end
+
 	/// <summary>
 	/// セット数
 	/// </summary>
@@ -343,7 +371,7 @@ private:
 	/// <summary>
 	/// 動物をファイルに保存
 	/// </summary>
-	void SaveAnimalToFile();
+	void SaveAnimalToFile(FPlayerSaveGame* Data);
 
 	/// <summary>
 	/// 一ゲーム内でついてくる動物
@@ -395,5 +423,28 @@ private:
 	/// すべてのデータを読み込む
 	/// </summary>
 	void ReadAll();
+
+	/// <summary>
+	/// セーフ完了
+	/// </summary>
+	/// <param name="SlotName">ファイル</param>
+	/// <param name="UserIndex"></param>
+	/// <param name="bResult">保存結果</param>
+	UFUNCTION()
+	void OnSaveComplete(const FString& SlotName, const int32 UserIndex, bool bResult);
+
+	/// <summary>
+	/// ロード完了
+	/// </summary>
+	/// <param name="SlotName">ファイル</param>
+	/// <param name="UserIndex"></param>
+	/// <param name="Data">ロードデータ</param>
+	UFUNCTION()
+	void OnLoadComplete(const FString& SlotName, const int32 UserIndex, USaveGame* Data);
+
+	/// <summary>
+	/// ファイル名
+	/// </summary>
+	FString FileName;
 #pragma endregion
 };
---
+++ Source/BicycleProject/Public/WuBranch/Struct/PlayerSaveGame.h
@@ -0,0 +1,97 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerSaveGame.generated.h"
+
+/// <summary>
+/// 動物関連のデータ
+/// </summary>
+USTRUCT(BlueprintType)
+struct BICYCLEPROJECT_API FAnimalDatas
+{
+    GENERATED_BODY()
+
+    /// <summary>
+    /// 持っている数
+    /// </summary>
+    UPROPERTY()
+    int32 Count = 0;
+
+    /// <summary>
+    /// 解放状況
+    /// </summary>
+    UPROPERTY()
+    float UnlockProgress = 0.0f; 
+};
+
+/**
+ *
+ */
+USTRUCT(BlueprintType)
+struct BICYCLEPROJECT_API FPlayerSaveGame
+{
+    GENERATED_BODY()
+
+    /// <summary>
+    /// 持っているコイン数
+    /// </summary>
+    UPROPERTY()
+    int32 Coins = 0;
+
+    /// <summary>
+    /// 動物関連のデータ
+    /// Map<動物ID, データ>
+    /// </summary>
+    UPROPERTY()
+    TMap<int32, FAnimalDatas> AnimalData;
+
+    /// <summary>
+    /// 全音量
+    /// </summary>
+    UPROPERTY()
+    float TotalVolume = 1.0f;
+
+    /// <summary>
+    /// BGM音量
+    /// </summary>
+    UPROPERTY()
+    float BGMVolume = 1.0f;
+
+    /// <summary>
+    /// SE音量
+    /// </summary>
+    UPROPERTY()
+    float SEVolume = 1.0f;
+
+    /// <summary>
+    /// 動物の鳴き音量
+    /// </summary>
+    UPROPERTY()
+    float VoiceVolume = 1.0f;
+
+    /// <summary>
+    /// 動物の足音の音量
+    /// </summary>
+    UPROPERTY()
+    float FootSEVolume = 1.0f;
+
+    /// <summary>
+    /// マップのセット数
+    /// </summary>
+    UPROPERTY()
+    int32 MapSets = 4;
+
+    /// <summary>
+    /// 制限した回転数
+    /// </summary>
+    UPROPERTY()
+    float RPMLimit = 10.0f;
+
+    /// <summary>
+    /// 回転数の閾値
+    /// </summary>
+    UPROPERTY()
+    int RPMThreshold = 10;
+};

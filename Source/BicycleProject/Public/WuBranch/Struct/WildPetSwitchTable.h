52e1c5f72c87c9fac4421a47d663f091220c9139
gene <cg100111@gmail.com>
2025-12-25 19:13:01 +0900

    森マップでは次生成のtileを修正 忘れた変換表のテーブル

---
+++ Source/BicycleProject/Public/WuBranch/Struct/WildPetSwitchTable.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WildPetSwitchTable.generated.h"
+
+/**
+ * 野生動物からペットへの変換表
+ */
+USTRUCT(BlueprintType)
+struct BICYCLEPROJECT_API FWildPetSwitchTable : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	/// <summary>
+	/// 野生動物のID
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	int32 WildAnimalID;
+
+	/// <summary>
+	/// ペットのID
+	/// </summary>
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	int32 PetAnimalID;
+};
